6a66829921e92f017313b95fb676f1e4f7954fba
tamilvanansambasivam <tamilvanansambasivam@gmail.com>
2023-06-06 10:18:50 +0530

    add some comments

--- day-2/main.js
+++ day-2/main.js
@@ -2,3 +2,30 @@
 //js is awesome 
 
 console.log("Hello from main.js!");
+
+// DATA TYPES (primitive and non-primitive)
+
+//PRIMITIVE
+
+//string
+
+const name="tamilvanan";
+const title='programmer';
+const about =`perpetual learner`;
+
+//number
+
+const age =20;
+const PI =3.14;
+
+//boolean 
+const isReal=true;
+const isPrime=false;
+
+//undefined
+let think;
+console.log(think);
+
+//null
+const nullData=null;
+console.log(nullData)
