1994e47d05f2aa6d3b500cc25abfe6ca31812c98
tamilvanansambasivam <tamilvanansambasivam@gmail.com>
2023-06-08 14:26:29 +0530

    code is slightly changed

--- day-3/main.js
+++ day-3/main.js
@@ -1,15 +1,43 @@
-let a = 10;
-//neested function
+// let a = 10;
+// //NESTED FUNCTION
+// function outer() {
+//     let b = 20;
+
+//     function inner() {
+//         var c = 30;
+//         console.log(a, b, c);
+
+//     }
+
+//     inner();
+// }
+
+// outer();
+
+//CLOSURE
+
+// function outer(){
+//     let counter=0;
+//     function inner(){
+//         counter++;
+//         console.log(counter);
+//     }
+//     inner();
+// }
+// outer();outer();
+
+
 function outer() {
-    let b = 20;
-
+    let counter = 0;
     function inner() {
-        var c = 30;
-        console.log(a, b, c);
-
+        counter++;
+        console.log(counter);
     }
-
-    inner();
+    return inner;
 }
 
-outer();
+const fn = outer();
+fn()
+fn()
+fn()
+
