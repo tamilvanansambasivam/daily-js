0fd4287eb0a11b3311c82da53c752c30932484bd
tamilvanansambasivam <tamilvanansambasivam@gmail.com>
2023-06-09 07:45:08 +0530

    practice typeof

---
+++ day-4/main.js
@@ -0,0 +1,23 @@
+
+//TYPEOF
+
+var a;
+typeof a;				// "undefined"
+
+a = "hello world";
+typeof a;				// "string"
+
+a = 42;
+typeof a;				// "number"
+
+a = true;
+typeof a;				// "boolean"
+
+a = null;
+typeof a;				// "object" -- weird, bug
+
+a = undefined;
+typeof a;				// "undefined"
+
+a = { b: "c" };
+typeof a;				// "object"
