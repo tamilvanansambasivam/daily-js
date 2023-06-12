19945b2bfd0adf5d87242a29981a1c6419444c3d
tamilvanansambasivam <tamilvanansambasivam@gmail.com>
2023-06-12 10:09:28 +0530

    practice array

--- day-7/main.js
+++ day-7/main.js
@@ -7,3 +7,19 @@ foo.bar = "hello world";
 console.log(typeof foo)	;	// "function"
 console.log(typeof foo());		// "number"
 console.log(typeof foo.bar);		// "string"
+
+//array
+
+
+var arr = [
+	"hello world",
+	42,
+	true
+];
+
+arr[0];			// "hello world"
+arr[1];			// 42
+arr[2];			// true
+arr.length;		// 3
+
+console.log(typeof arr);		// "object"
