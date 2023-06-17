0ac66cbbb8136db12fa470cf53812efea0eaa93e
tamilvanan <tamilvanan@mac.local>
2023-06-17 19:35:36 +0530

    practice

--- react/hello-world/src/components/Welcome.js
+++ react/hello-world/src/components/Welcome.js
@@ -1,7 +1,7 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
-class Welcome extends Component{
-    render(){
+class Welcome extends Component {
+    render() {
         return <h1>Welcome {this.props.name} a.k.a {this.props.heroName}</h1>
     }
 
