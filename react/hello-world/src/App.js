df8c665e30801afd4d5d9293111b07b5615b351a
tamilvanan <tamilvanan@mac.local>
2023-06-18 19:18:44 +0530

    practice

--- react/hello-world/src/App.js
+++ react/hello-world/src/App.js
@@ -3,18 +3,20 @@ import './App.css';
 import Greet from './components/Greet'
 import Welcome from './components/Welcome'
 import Hello from './components/Hello'
+import Message from './components/Message'
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
-      
-      
-      
+      <Message/>
+      <Counter/>
       
  
-      <Greet name='Tony Stark' heroName='Iron Man'>
+      {/* <Greet name='Tony Stark' heroName='Iron Man'>
         <p>This is children props</p>
       </Greet>
+
       <Greet name='Bruce Wayne' heroName='Bat Man' >
         <button>
           Action
@@ -23,7 +25,7 @@ function App() {
 
       <Welcome name='Tony Stark' heroName='Iron Man'/>
       <Welcome name='Tony Stark' heroName='Iron Man'/>
-      
+       */}
     
     </div>
     ); 
