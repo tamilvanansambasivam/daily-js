dbe090c4cd789139d39741933ed09ad3d610af89
tamilvanan <61337194+tamilvanansambasivam@users.noreply.github.com>
2023-10-13 23:40:44 +0530

    Update main.js

--- day-7/main.js
+++ day-7/main.js
@@ -1,25 +1,6 @@
-function foo() {
-	return 42;
-}
-
-foo.bar = "hello world";
-
-console.log(typeof foo)	;	// "function"
-console.log(typeof foo());		// "number"
-console.log(typeof foo.bar);		// "string"
-
-//array
-
-
-var arr = [
-	"hello world",
-	42,
-	true
-];
-
-arr[0];			// "hello world"
-arr[1];			// 42
-arr[2];			// true
-arr.length;		// 3
-
-console.log(typeof arr);		// "object"
+async function sayHello() {
+    return "Hello, World!";
+  }
+  
+  
+sayHello().then(res => console.log(res));
