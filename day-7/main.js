e34500f31b280ce04d8e6044b6954826d4c5320c
tamilvanan <61337194+tamilvanansambasivam@users.noreply.github.com>
2023-10-14 22:17:17 +0530

    Update main.js

--- day-7/main.js
+++ day-7/main.js
@@ -1,6 +1,27 @@
-async function sayHello() {
-    return "Hello, World!";
-  }
-  
-  
-sayHello().then(res => console.log(res));
+const p1= new Promise(function(res, rej){
+  setTimeout(()=>{
+    res("promise resolved")
+  },20000)
+})
+
+
+const p2= new Promise(function(res, rej){
+  setTimeout(()=>{
+    res("promise resolved")
+  },40000)
+})
+
+async function handlePromise(){
+  console.log("hello coder")
+  const value=await p1;
+  console.log(value)
+
+
+  console.log("hello coder ")
+  const value2=await p2;
+  console.log(value2)
+
+
+}
+
+handlePromise()
