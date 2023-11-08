7d6d7f6e96ebf147732fe18f246a71426205078a
tamilvanan <61337194+tamilvanansambasivam@users.noreply.github.com>
2023-11-08 11:11:58 +0530

    Update helloWorld.js

--- day-1/helloWorld.js
+++ day-1/helloWorld.js
@@ -1,7 +1,1 @@
 console.log("Hello World !");
-
-/*
-"Hello, World!" serves as a friendly introduction to programming, 
-helping beginners get started and build confidence in their ability to write and run code. 
-It's a simple yet symbolic way to say, "I can make the computer do something! 
-*/
